17678e965e2d6993903bc02224b2e4e6ba63ba5f
Jazib Bashir <jazibbashir@gmail.com>
2020-09-14 23:41:45 +0500

    excel_parser - implmented parse into json functionlaity using serde and calamine

--- jazib_bashir/excel_to_json_dictionary/src/main.rs
+++ jazib_bashir/excel_to_json_dictionary/src/main.rs
@@ -1,9 +1,12 @@
 pub mod cli_manager;
+pub mod excel_parser;
 
 use cli_manager::Args;
 
 fn main() {
+	// get file argument from cli manager
 	let args = Args::parse();
 	let Args { file } = args;
-	println!("File Path! {:?}", file);
+	// pass file to excel parser for json dictionary
+	excel_parser::parse_into_json(&file);
 }
